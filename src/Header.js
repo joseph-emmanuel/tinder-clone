30e84c69e3cb38ba51557a1e1e2f46b7d565936e
joseph-emmanuel <joseph.emmanuel@outlook.com>
2021-01-16 22:12:22 -0500

    Added files foir the basic header functionality and masks for the peoject

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.cs'
+function Header() {
+    return (
+        <div className='header'>
+            <h1>I am the header</h1>
+        </div>
+    )
+}
+
+export default Header
