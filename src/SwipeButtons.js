02a5121f4935d597c8ff4b6c2b19d084ba129928
joseph-emmanuel <joseph.emmanuel@outlook.com>
2021-01-17 01:52:39 -0500

    completed frontend

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,10 +1,32 @@
 import React from 'react'
-
 import "./SwipeButtons.css"
+
+import ReplayIcon from "@material-ui/icons/Replay"
+import CloseIcon from "@material-ui/icons/Close"
+import StarRateIcon from "@material-ui/icons/StarRate"
+import FavoriteIcon from "@material-ui/icons/Favorite"
+import FlashOnIcon from "@material-ui/icons/FlashOn"
+import IocnButton from "@material-ui/core/IconButton"
+
+
 function SwipeButtons() {
     return (
-        <div className="swipeButtons">
-            
+        <div className="swipeButtons">            
+            <IocnButton className="swipeButtons_repeat">
+                <ReplayIcon fontSize="large"/>                
+            </IocnButton>
+            <IocnButton className="swipeButtons_left">
+                <CloseIcon fontSize="large"/>                
+            </IocnButton>
+            <IocnButton className="swipeButtons_star">
+                <StarRateIcon fontSize="large"/>                
+            </IocnButton>
+            <IocnButton className="swipeButtons_right">
+                <FavoriteIcon fontSize="large"/>                
+            </IocnButton>
+            <IocnButton className="swipeButtons_lightning">
+                <FlashOnIcon fontSize="large"/>                
+            </IocnButton>
         </div>
     )
 }
